d3df3ce160922020fbc10599e58a0ce6e12c36f3
mike-duvall <mike.a.duvall@gmail.com>
2023-03-02 07:43:22 -0600

    Cleanup

--- src/test/groovy/client/SequentialEventReader.groovy
+++ src/test/groovy/client/SequentialEventReader.groovy
@@ -1,5 +1,6 @@
 package client
 
+import domain.event.EventType
 import domain.event.SimulationStateUpdateEvent
 import spock.util.concurrent.PollingConditions
 
@@ -49,13 +50,17 @@ class SequentialEventReader {
 
             }
 
-//            assert (foundEvent != null) && (eventType == foundEvent.eventType)
             assert (foundEvent != null)
-
-
         }
 
         return foundEvent
+
+    }
+
+    def eventMatcher(String eventType) {
+        return {SimulationStateUpdateEvent e ->
+            return e.eventType == eventType
+        }
 
     }
 
@@ -63,35 +68,37 @@ class SequentialEventReader {
 
     SimulationStateUpdateEvent waitForEventOfType(String eventType) {
 
-        int timeoutInSeconds = 30
-        SimulationStateUpdateEvent foundEvent = null
+        return waitForEventMatchedBy (eventMatcher(eventType))
 
-        def conditions = new PollingConditions(timeout: timeoutInSeconds, initialDelay: 0.3, factor: 1.25)
-        conditions.eventually {
-
-            List<SimulationStateUpdateEvent> latestEventList = simulationClient.getSimulationStateUpdateEvents(allReceivedEvents.size())
-            allReceivedEvents.addAll(latestEventList)
-
-            boolean done = indexOfFurthestEvaluatedEvent >= allReceivedEvents.size()
-            while(!done) {
-                SimulationStateUpdateEvent nextEventToEvaluate = allReceivedEvents.get(indexOfFurthestEvaluatedEvent)
-                indexOfFurthestEvaluatedEvent++
-
-                if(nextEventToEvaluate.eventType == eventType) {
-                    foundEvent = nextEventToEvaluate
-                    done = true
-                }
-                else {
-                    done = indexOfFurthestEvaluatedEvent >= allReceivedEvents.size()
-                }
-            }
-
-            assert (foundEvent != null) && (eventType == foundEvent.eventType)
-
-
-        }
-
-        return foundEvent
+//        int timeoutInSeconds = 30
+//        SimulationStateUpdateEvent foundEvent = null
+//
+//        def conditions = new PollingConditions(timeout: timeoutInSeconds, initialDelay: 0.3, factor: 1.25)
+//        conditions.eventually {
+//
+//            List<SimulationStateUpdateEvent> latestEventList = simulationClient.getSimulationStateUpdateEvents(allReceivedEvents.size())
+//            allReceivedEvents.addAll(latestEventList)
+//
+//            boolean done = indexOfFurthestEvaluatedEvent >= allReceivedEvents.size()
+//            while(!done) {
+//                SimulationStateUpdateEvent nextEventToEvaluate = allReceivedEvents.get(indexOfFurthestEvaluatedEvent)
+//                indexOfFurthestEvaluatedEvent++
+//
+//                if(nextEventToEvaluate.eventType == eventType) {
+//                    foundEvent = nextEventToEvaluate
+//                    done = true
+//                }
+//                else {
+//                    done = indexOfFurthestEvaluatedEvent >= allReceivedEvents.size()
+//                }
+//            }
+//
+//            assert (foundEvent != null) && (eventType == foundEvent.eventType)
+//
+//
+//        }
+//
+//        return foundEvent
 
     }
 
